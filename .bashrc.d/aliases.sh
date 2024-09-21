8aef8155ac68fabb5fbe4e258af5c8cda3644df5
Yuchan Lee <lasuillard@gmail.com>
2024-09-21 12:30:12 +0000

    chore: Common aliases

---
+++ .bashrc.d/aliases.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+alias ans='ansible'
+alias cdd='source cdd.sh'
+alias h='helm'
+alias ic='istioctl'
+alias K='k9s'
+alias k='kubectl'
+alias kk='kubectl kustomize'
+alias mk='minikube'
+alias tf='tofu'
+alias tg='terragrunt'
+alias tp='telepresence'
--- .bashrc.d/common.sh
+++ .bashrc.d/common.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
 export PATH="${PATH}:~/bin/shell"
-
-alias cdd='source cdd.sh'
