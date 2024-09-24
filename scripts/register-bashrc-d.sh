1e502a1164fe8e701e8fd59ef18eec9cad95d499
Yuchan Lee <lasuillard@gmail.com>
2024-09-24 14:11:11 +0000

    chore: Separate .bashrc.d registration script with better snippet replace

---
+++ scripts/register-bashrc-d.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+begin="#bashrc-d-register@start"
+end="#bashrc-d-register@end"
+
+# Check if the snippet is already in the .bashrc
+n_begin=$(awk "/${begin}/{print NR; exit}" ~/.bashrc)
+n_end=$(awk "/${end}/{print NR; exit}" ~/.bashrc)
+
+# Remove the old snippet
+sed -i "${n_begin},${n_end}d" ~/.bashrc
+
+# Add the new snippet
+cat <<EOT >> ~/.bashrc
+$begin
+for f in ~/.bashrc.d/*.sh; do
+    source "\$f"
+done
+$end
+EOT
