f87b229c81b197adb0c001bb0f2ff28544cd4e34
Yuchan Lee <lasuillard@gmail.com>
2024-11-30 14:48:41 +0000

    chore: Add helpful aliases more

--- .bashrc.d/aliases.sh
+++ .bashrc.d/aliases.sh
@@ -1,20 +1,73 @@
 #!/usr/bin/env bash
 
+# https://www.cyberciti.biz/tips/bash-aliases-mac-centos-linux-unix.html
+
+# Navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+
+# Utilities
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+alias mkdir='mkdir -pv'
+if command -v colordiff &> /dev/null; then
+    alias diff='colordiff'
+fi
+
+# Continue downloading
+alias wget='wget -c'
+
+# Network
+alias ports='netstat -tulanp'
+
+# Custom scripts
 alias cdd='source cdd.sh'
 
-alias ans='ansible'
-alias d='docker'
-alias gc='gcloud'
-alias h='helm'
-alias ic='istioctl'
-alias K='k9s'
-alias k='kubectl'
-alias kk='kubectl kustomize'
-alias mk='minikube'
-alias pl='pulumi'
-alias pls='pulumi stack'
-alias tal='talosctl'
-alias tf='tofu'
-alias tg='terragrunt'
-alias tp='telepresence'
-alias vg='vagrant'
+# External utilities but do not set alias if hiding existing command
+declare -A aliases
+
+aliases=(
+    [ans]='ansible'
+    [d]='docker'
+    [gc]='gcloud'
+    [h]='helm'
+    [ic]='istioctl'
+    [K]='k9s'
+    [k]='kubectl'
+    [kk]='kubectl kustomize'
+    [mk]='minikube'
+    [pl]='pulumi'
+    [pls]='pulumi stack'
+    [tal]='talosctl'
+    [tf]='tofu'
+    [tg]='terragrunt'
+    [tp]='telepresence'
+    [vg]='vagrant'
+)
+
+for key in "${!aliases[@]}"; do
+    # Skip if the alias is overwriting an existing command
+    if command -v "${aliases[$key]}" &> /dev/null; then
+        continue
+    fi
+
+    # shellcheck disable=SC2139
+    alias "$key"="${aliases[$key]}"
+done
+
+# Safety nets
+# ----------------------------------------------------------------------------
+# Do not delete / or prompt if deleting more than 3 files at a time
+alias rm='rm -I --preserve-root'
+
+# Ask confirmation
+alias mv='mv -i'
+alias cp='cp -i'
+alias ln='ln -i'
+
+# Parenting changing perms on /
+alias chown='chown --preserve-root'
+alias chmod='chmod --preserve-root'
+alias chgrp='chgrp --preserve-root'
