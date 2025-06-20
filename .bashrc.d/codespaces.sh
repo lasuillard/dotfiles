80900f54748fd369b34b929f85cde344d18a8448
Yuchan Lee <lasuillard@gmail.com>
2025-06-21 00:02:13 +0900

    Update codespaces.sh
    Signed-off-by: Yuchan Lee <lasuillard@gmail.com>

--- .bashrc.d/codespaces.sh
+++ .bashrc.d/codespaces.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
 # FIXME: Temporary workaround for SSH configuration conflicts with GPG in GitHub Codespaces
-if [ ! "$CODESPACES" ]; then
-  exit 0
+if [ "$CODESPACES" ]; then
+  echo 'Current environment looks like GitHub Codespaces.'
+  echo 'Applying temporary workaround for Git SSH configuration with GPG...'
+  git config --global --unset user.signingkey
+  git config --global --unset gpg.format
+  git config --global --unset 'url.git@github.com:.insteadof'
 fi
-
-echo 'Current environment looks like GitHub Codespaces.'
-echo 'Applying temporary workaround for Git SSH configuration with GPG...'
-git config --global --unset user.signingkey
-git config --global --unset gpg.format
-git config --global --unset 'url.git@github.com:.insteadof'
