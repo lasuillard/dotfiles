ea6fa35b2aa9b1966143aafda062b93a5b06e4d4
Yuchan Lee <lasuillard@gmail.com>
2025-06-23 15:56:16 +0000

    feat: Add empty `dotfiles.bash` file for later convenience CLI commands

--- profile/.bashrc.d/000-common.bash
+++ profile/.bashrc.d/000-common.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-export PATH="${PATH}:~/.bin/shell"
+export PATH="${PATH}:~/.bin:~/.bin/shell"
 
 # Custom scripts
+alias dotfiles='dotfiles.bash'
 alias cdd='source cdd.bash'
---
+++ profile/.bin/dotfiles.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# TODO: This script will be used to provide some convenience CLI commands,
+#       for example, syncing dotfiles up-to-date.
+echo "Hello, World!"
