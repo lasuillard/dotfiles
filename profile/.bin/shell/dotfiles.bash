bc280a95f7b542762ce39d689ba933e38d904de0
Yuchan Lee <lasuillard@gmail.com>
2026-01-19 09:54:56 +0000

    fix: inconsistent behavior of `dotfiles` command

--- profile/.bin/shell/dotfiles.bash
+++ profile/.bin/shell/dotfiles.bash
@@ -4,12 +4,18 @@
 Dotfiles management script.
 '
 
+root_dir="$(realpath "$(dirname "$0")/../../..")"
+
 function show_logs() {
-  tail --lines 1000 ./logs/install.log | more
+  log_dir="${root_dir}/logs"
+  if [ ! -d "${log_dir}" ]; then
+    echo "Log directory ${log_dir} does not exist."
+    exit 1
+  fi
+  tail --lines 1000 "${log_dir}/install.log" | more
 }
 
 function update() {
-  root_dir="$(realpath "$(dirname "$0")/../..")"
   cd "$root_dir" || exit 1
   echo "Updating dotfiles (${root_dir}) from repository..."
   git pull --rebase
