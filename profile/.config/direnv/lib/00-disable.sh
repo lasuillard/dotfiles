b5cf57312d6e51b4350f204336780dfbdd6e3779
Yuchan Lee <lasuillard@gmail.com>
2026-01-19 10:16:08 +0000

    feat: direnv extension to disable direnv temporarily via `DIRENV_DISABLE` variable

---
+++ profile/.config/direnv/lib/00-disable.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# https://github.com/direnv/direnv/issues/550
+if [ -n "${DIRENV_DISABLE-}" ]; then
+  echo "direnv is disabled via DIRENV_DISABLE"
+  exit
+else
+  cat <<EOT
+direnv is enabled. To disable direnv temporarily, set \$DIRENV_DISABLE:
+
+export DIRENV_DISABLE=1 && direnv reload
+
+to re-enable it, run:
+
+export DIRENV_DISABLE= && direnv reload
+
+EOT
+fi
