372caf88a5695b43bf8c30281668f3d5c27b4e64
Yuchan Lee <lasuillard@gmail.com>
2026-05-11 18:17:30 +0900

    Enhance feedback for linking and copying setup files
    Added echo statements to provide feedback when linking and copying files.
    
    Signed-off-by: Yuchan Lee <lasuillard@gmail.com>

--- profile/.bin/shell/unexample.sh
+++ profile/.bin/shell/unexample.sh
@@ -6,15 +6,19 @@ if the target file/directory does not exist.
 '
 
 if [ ! -d '.devcontainer' ]; then
+  echo 'Linking .devcontainer to .devcontainer.example'
   ln --symbolic .devcontainer.example .devcontainer
 fi
 if [ ! -d '.vscode' ]; then
+  echo 'Linking .vscode to .vscode.example'
   ln --symbolic .vscode.example .vscode
 fi
 if [ ! -f '.env' ]; then
+  echo 'Copying .env.example from .env'
   cp --verbose --update=none .env.example .env
 fi
 if [ ! -f '.envrc' ]; then
+  echo 'Creating new .envrc file'
   cat <<ENVRC >.envrc
 # Uncomment below to load .env file automatically
 # See https://direnv.net/man/direnv-stdlib.1.html for other useful functions
