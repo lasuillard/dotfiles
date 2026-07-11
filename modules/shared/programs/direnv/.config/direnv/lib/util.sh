baa1cb9c709d6eb4d26dd63b3654cb57dfe2753c
Yuchan Lee <lasuillard@gmail.com>
2026-07-11 04:49:25 +0000

    Add direnv util `snake_case()`

--- modules/shared/programs/direnv/.config/direnv/lib/util.sh
+++ modules/shared/programs/direnv/.config/direnv/lib/util.sh
@@ -16,6 +16,12 @@ slugify() {
     sed --regexp-extended 's/^-+|-+$//g'
 }
 
+snake_case() {
+  echo "$1" |
+    sed --regexp-extended 's/[^[:alnum:]]+/_/g' |
+    sed --regexp-extended 's/^_+|_+$//g'
+}
+
 gh_pr_number() {
   pr_number="$(gh pr view --json number --jq '.number' 2>/dev/null || echo 'X')"
   echo "$pr_number"
