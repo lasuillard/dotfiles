84cb857e9372e19c99a559269a58e6d587a22fd6
Yuchan Lee <lasuillard@gmail.com>
2026-07-11 03:40:23 +0000

    Add direnv util `slugify()` and `gh_pr_number()`

--- modules/shared/programs/direnv/.config/direnv/lib/util.sh
+++ modules/shared/programs/direnv/.config/direnv/lib/util.sh
@@ -1,4 +1,4 @@
-# shellcheck disable=SC2148
+# shellcheck shell=sh
 
 is_devcontainer() {
   if [ "$REMOTE_CONTAINERS" = "true" ] || [ "$CODESPACES" = "true" ]; then
@@ -7,4 +7,16 @@ is_devcontainer() {
   fi
   echo "false"
   return
+}
+
+slugify() {
+  echo "$1" |
+    tr '[:upper:]' '[:lower:]' |
+    sed --regexp-extended 's/[^[:alnum:]]+/-/g' |
+    sed --regexp-extended 's/^-+|-+$//g'
+}
+
+gh_pr_number() {
+  pr_number="$(gh pr view --json number --jq '.number' 2>/dev/null || echo 'X')"
+  echo "$pr_number"
 }
