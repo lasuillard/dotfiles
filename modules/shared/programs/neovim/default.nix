785ce2aea10f33dbbba404e1f894c047b69f15a0
Yuchan Lee <lasuillard@gmail.com>
2026-08-17 11:51:30 +0000

    Manage Neovim configuration and plugins with Nixvim

--- modules/shared/programs/neovim/default.nix
+++ modules/shared/programs/neovim/default.nix
@@ -1,16 +1,34 @@
 { ... }:
 {
-  programs.neovim = {
+  # Configuration examples: https://nix-community.github.io/nixvim/user-guide/config-examples.html
+  programs.nixvim = {
     enable = true;
+
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
-  };
 
-  home.file = {
-    ".config/nvim" = {
-      source = ./.config/nvim;
-      recursive = true;
+    # https://github.com/topics/neovim-colorscheme
+    colorschemes.catppuccin.enable = true;
+
+    opts = {
+      number = true;
+      relativenumber = true;
+    };
+
+    editorconfig.enable = true;
+    plugins = {
+      lualine.enable = true;
+
+      treesitter = {
+        enable = true;
+        nixvimInjections = true;
+
+        settings = {
+          highlight.enable = true;
+          indent.enable = true;
+        };
+      };
     };
   };
 }
